b8443d5cacae9607ef2c23388a19292b81ba8135
houdinimain <houdgondal@icloud.com>
2024-12-11 19:00:11 +0000

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,86 @@
+import pgzrun
+def draw ():
+    screen.clear()
+    screen.fill("blue")
+    screen.draw.circle((120,120),50,"pink")
+    screen.draw.filled_circle((100,100),50,"pink")#
+    
+    rectangle = Rect((200,200),(200,200))
+    
+
+    screen.draw.rect(rectangle,"red")
+
+
+ 
+    rectangle2 = Rect((100,200),(100,200))
+    
+
+    screen.draw.filled_rect(rectangle2,"red")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pgzrun.go()
